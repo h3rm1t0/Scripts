9641fc7720c06ea450a8162f2d93e2e3b28bec9a
h3rm1t0 <osmar.almeidamiotto@gmail.com>
2025-05-20 00:06:20 -0300

    Create ping_sweap.ps1

---
+++ powershell./ping_sweap.ps1
@@ -0,0 +1,21 @@
+#Simple ping sweap that can return all local hosts in one small and local subnet
+
+param($ip)
+
+if(!$ip){
+
+    echo ""
+    echo "Insira no primeiro argumento o IP do alvo."
+    echo "Exemplo: .\pingador_v2 192.168.0.1"
+
+} else {
+    echo "Varrendo sub-rede $ip ..."
+    echo ""
+    
+    foreach ($var1 in 1..254){
+        try {$output = $output2 = ping -n 1 "$ip.$var1" | Select-String "bytes=32"
+            $output.Line.split(' ')[2] -replace ":",""
+            $output2.Line.split(' ')[5] -replace "TTL=",""
+    }catch {}
+    }
+}
