6ab24f93222e87a51493ef8a3a2b9708cb43a537
h3rm1t0 <osmar.almeidamiotto@gmail.com>
2025-06-02 22:59:25 -0400

    Create interact.py

---
+++ python3./interact.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+import socket
+import sys
+
+
+if ((len(sys.argv) - 1) < 1):
+
+        print ('[ATENÇÃO]: O script exige duas entradas.')
+        print ('[FUNÇÃO]: Realiza banner grabbing em uma lista de IP em uma porta de interesse.')
+        print (f'[EXEMPLO]: ./interact.py <porta> <Caminho absoluto de uma litsa de IPs>')
+
+else:
+
+        ip_list =  sys.argv[2]
+        p = int(sys.argv[1])
+
+        with open(ip_list, 'r') as file:
+                for line in file:
+
+                        ip = line.strip()
+                        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+                        print (f'++++ BANNER GRABBING {ip}:{p} ++++')
+
+                        if sock.connect_ex((ip,p)) == 0:
+                                banner = sock.recv(1024)
+                                print (banner)
+                                msg = 'AAA'
+                                sock.send(str.encode(msg))
+                                banner = sock.recv(1024)
+                                print (banner)
+                                msg = 'BBB'
+                                sock.send(str.encode(msg))
+                                banner = sock.recv(1024)
+                                print (banner)
+                                sock.close()
+                        else:
+                                print ('SERVIÇO INDISPONÍVEL OU CONEXÃO NEGADA')
+                                sock.close()
+
+
